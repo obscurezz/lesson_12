ca09b9802c69721f5d70cfcd9fa6f9f988c6ca53
Andrey Kopylov <104315220+obscurezz@users.noreply.github.com>
2022-06-17 19:03:18 +0300

    refactored some code, images still don't work on my windows

--- loader/loader.py
+++ loader/loader.py
@@ -6,13 +6,10 @@ from jinja2 import TemplateNotFound
 
 from functions import JsonLoader, create_content_dict, is_filename_allowed
 
-# Log configuration settings
-# FORMAT = '%(asctime)s %(clientip)-15s %(message)s'
-logging.basicConfig(format='%(levelname)s:%(asctime)s:%(message)s', filename="logs/loader.log", level=logging.DEBUG)
 
 post_blueprint = Blueprint('post_blueprint', __name__, template_folder="templates")
 POSTS = JsonLoader('posts.json')
-UPLOAD_FOLDER = "/uploads/images"
+UPLOAD_FOLDER = "./uploads/images"
 
 
 @post_blueprint.route('/post')
@@ -20,23 +17,28 @@ def page_post_form():
     return render_template('post_form.html')
 
 
-@post_blueprint.route('/post/upload', methods=["GET", "POST"])
+@post_blueprint.route('/post', methods=["GET", "POST"])
 def page_post_upload():
     try:
+        # Text data from html form to json
         content = request.form.get('content')
+
+        # Image data from html to json
         picture = request.files.get('picture')
         picture_filename = picture.filename
 
+        # Check if uploaded file is not an image
         if is_filename_allowed(picture_filename):
             picture.save(f"{UPLOAD_FOLDER}/{picture_filename}")
         else:
             logging.info(f'Extension {picture_filename.split(".")[-1]} is not allowed')
             return f'Extension {picture_filename.split(".")[-1]} is not allowed'
 
-        uploader = create_content_dict(f"{UPLOAD_FOLDER}/{picture_filename}", content)
-        POSTS.upload_data_to_json(uploader)
+        # Creating a dict and uploading it to json
+        post: dict = create_content_dict(f"{UPLOAD_FOLDER}/{picture_filename}", content)
+        POSTS.upload_data_to_json(post)
 
-        return render_template('post_uploaded.html', picture=f"{UPLOAD_FOLDER}/{picture_filename}", content=content)
+        return render_template('post_uploaded.html', post=post)
     except TemplateNotFound:
         logging.info("Template was not found")
         abort(404)
