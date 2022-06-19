30b3963838a6b9f6d2c5b88637d768987fb82aad
Andrey Kopylov <104315220+obscurezz@users.noreply.github.com>
2022-06-19 15:48:00 +0300

    added static dir route for returning images from uploads/images, added creating logs directory if not exists

--- app.py
+++ app.py
@@ -1,13 +1,23 @@
 import logging
-from flask import Flask
+from flask import Flask, send_from_directory
 from loader.loader import post_blueprint
 from main.main import main_blueprint
+from os import path, mkdir
+from settings import LOGS_FOLDER
 
-logging.basicConfig(format='%(levelname)s:%(asctime)s:%(message)s', filename="logs/main.log", level=logging.DEBUG)
+if not path.exists(LOGS_FOLDER):
+  mkdir(LOGS_FOLDER)
+
+FORMAT = '%(levelname)s:%(asctime)s - - %(message)s'
+logging.basicConfig(format=FORMAT, encoding='utf-8', filename="logs/main.log", level=logging.DEBUG)
 
 app = Flask(__name__)
 
 app.register_blueprint(main_blueprint)
 app.register_blueprint(post_blueprint)
+
+@app.route("/uploads/images/<path:path>")
+def static_dir(path):
+    return send_from_directory("uploads/images", path)
 
 app.run()
--- settings.py
+++ settings.py
@@ -2,6 +2,8 @@ from pathlib import Path
 
 BASE_DIR: str = Path(__file__).resolve().parent
 
+LOGS_FOLDER: str = BASE_DIR.joinpath('logs')
+
 UPLOAD_FOLDER: str = BASE_DIR.joinpath('uploads')
 
 UPLOAD_IMAGES_DIR: str = UPLOAD_FOLDER.joinpath('images')
